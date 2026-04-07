41553f45b7c6bec890a61567c026e152329e8b68
Rego08 <regomark866@gmail.com>
2026-04-07 11:16:44 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,9 @@
 </div>
 
+<footer class="bg-dark text-white text-center p-3 mt-4">
+    &copy; <?php echo date("Y"); ?> Fotostuudio | Täna: <?php echo date("d.m.Y"); ?>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
