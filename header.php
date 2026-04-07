d351ded94e20408489a9a69e0bbd55d669eb6704
Rego08 <regomark866@gmail.com>
2026-04-07 10:29:59 +0300

    Update header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fotostuudio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+    <a class="navbar-brand" href="index.php">📷 Fotostuudio</a>
+
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link" href="index.php">Avaleht</a></li>
+            <li class="nav-item"><a class="nav-link" href="teenused.php">Teenused</a></li>
+            <li class="nav-item"><a class="nav-link" href="kalkulaator.php">Kalkulaator</a></li>
+            <li class="nav-item"><a class="nav-link" href="kontakt.php">Kontakt</a></li>
+            <li class="nav-item"><a class="nav-link" href="ostukorv.php">Ostukorv</a></li>
+        </ul>
+    </div>
+</div>
+</nav>
+
+<div class="container mt-4">
