19cd85fa2921f423b5bf842e038049c2e98618a8
Rego08 <regomark866@gmail.com>
2026-04-20 13:09:43 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,30 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang="et">
 <head>
     <meta charset="UTF-8">
-    <title>Fotostuudio</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Minu PHP projekt</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container">
-    <a class="navbar-brand" href="index.php">📷 Fotostuudio</a>
-
-    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="menu">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item"><a class="nav-link" href="index.php">Avaleht</a></li>
-            <li class="nav-item"><a class="nav-link" href="teenused.php">Teenused</a></li>
-            <li class="nav-item"><a class="nav-link" href="kalkulaator.php">Kalkulaator</a></li>
-            <li class="nav-item"><a class="nav-link" href="kontakt.php">Kontakt</a></li>
-            <li class="nav-item"><a class="nav-link" href="ostukorv.php">Ostukorv</a></li>
-        </ul>
-    </div>
-</div>
+<nav>
+    <a href="index.php">Avaleht</a>
+    <a href="index.php?page=kontakt">Kontakt</a>
+    <a href="index.php?page=kalkulaator">Kalkulaator</a>
+    <a href="index.php?page=ostukorv">Ostukorv</a>
 </nav>
 
-<div class="container mt-4">
+<hr>
